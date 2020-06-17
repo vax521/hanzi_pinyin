877d243fc6c3859874d372d95f0b91084791b6f1
Xiaofei <13263397018@163.com>
2020-06-17 12:12:13 +0800

    增加tiny pinyin demo

---
+++ src/main/java/TinyPinyinHelper.java
@@ -0,0 +1,7 @@
+import com.github.promeg.pinyinhelper.Pinyin;
+
+public class TinyPinyinHelper {
+    public static void main(String[] args) {
+       System.out.println(Pinyin.toPinyin("单雄信",""));
+    }
+}
